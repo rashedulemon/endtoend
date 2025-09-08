383e792a93dc3feabfd2a4dcab0917d9d46e4183
Md. Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2025-09-08 20:19:12 +0600

    Add utility for object serialization and update data pipeline
    Introduced a new save_object utility in utils.py using dill for object serialization. Updated requirements.txt to include dill. Enhanced data_ingestion.py to trigger data transformation after ingestion. Improved data_transformation.py to separate input and target features and added logging for preprocessing steps. Added preprocessor.pkl artifact for storing preprocessing objects.

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -82,8 +82,16 @@ class DataTransformation:
 
             target_column_name = 'math_score'
             numerical_columns = ['writing_score', 'reading_score']
-            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
-            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
